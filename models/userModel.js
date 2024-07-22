cddb5756bdc30aa781b3c5d783691ec9ee45e7dd
Ktripathi2611 <tripathikushal522@gmail.com>
2024-07-23 01:51:56 +0530

    added  userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+import { Timestamp } from 'mongodb'
+
+const  mongoose = require('mongoose')
+const userschema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        phone:{
+            type:String,
+            required:true,
+        },
+        address:{
+            type:String,
+            required:true,
+        },
+    role:{
+        type:Number,
+        default:0
+    }
+ },{Timestamp:true})
+
+ export default mongoose.model('users',userschema)

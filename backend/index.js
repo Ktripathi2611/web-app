c42f995e940b6143829bb157775b13cb4af24ed8
Ktripathi2611 <tripathikushal522@gmail.com>
2024-07-30 20:58:27 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,39 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+// packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-// Importing connectDB utility
-import connectDB from './config/db.js';
-import userRoutes from './Routes/userRoutes.js'; // Assuming ES Modules
+// Utiles
+import connectDB from "./config/db.js";//database is conneted here 
+import userRoutes from "./Routes/userRoutes.js";
+import categoryRoutes from "./Routes/categoryRoutes.js";
+import productRoutes from "./Routes/productRoutes.js";
+import uploadRoutes from "./Routes/uploadRoutes.js";
+import orderRoutes from "./Routes/orderRoutes.js";
 
-// Load environment variables from .env file
 dotenv.config();
-
 const port = process.env.PORT || 5000;
+
+connectDB();
+
 const app = express();
 
-// Middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Connect to database
-connectDB();
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
 
-// Routes
-app.use('/api/users', userRoutes);
-
-// Start server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));

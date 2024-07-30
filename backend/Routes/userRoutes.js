90122d3fde2feaf4f3e141a8391eaede1a1826ae
Ktripathi2611 <tripathikushal522@gmail.com>
2024-07-30 20:57:02 +0530

    routes addded

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,9 +1,38 @@
-import express from 'express';
+import express from "express";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+} from "../Controllers/userController.js";
 
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddlware.js";
 
-import { createUser } from '../Controllers/userController.js'
-const router = express.Router()
+const router = express.Router();
 
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
 
-router.route('/').post(createUser)
-export default router
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
+
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
+
+// ADMIN ROUTES 👇
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
+
+export default router;
